18e372bf1ae5c8118815d1a4cf8ee70268fa971b
Marcos J. Alvarez <garusis@gmail.com>
2017-03-25 21:43:37 -0500

    Try to add environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+console.log("&&&&&&&&&&&&&&&&&&&&&&")
+console.log(process.env.NODE_ENV)
+console.log("&&&&&&&&&&&&&&&&&&&&&&")
+
 module.exports = {
   entry: "./src/app.js",
-  output:{
-    filename: "./dist/app.js"
-  }
+  output: {
+    path: path.join(process.cwd(), "dist"),
+    filename: "assets/app.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs'
+    })
+  ]
 }
